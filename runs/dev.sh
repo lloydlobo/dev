fb27ce6ddb937999d56ff4b47b95f0720e9f8713
Lloyd Lobo <lloydlobo4@gmail.com>
2025-02-12 15:55:12 +0530

    runs: add libs.sh and neovim.sh for commonly used apps

--- runs/dev.sh
+++ runs/dev.sh
@@ -6,3 +6,16 @@ git config --global user.email "lloydlobo4@gmail.com"
 git config --global user.name "Lloyd Lobo"
 
 cargo install stylua
+
+# Needs development installation of Lua5.1
+# luarocks installed in ./neovim.sh
+# pushd /tmp/luarocks-3.11.0
+# (
+#     wget --output-document /tmp/luarocks.tar.gz https://luarocks.org/releases/luarocks-3.11.0.tar.gz
+#     tar zxpf /tmp/luarocks.tar.gz -C /tmp
+#     cd
+#     ./configure && make && sudo make install
+# )
+# popd
+#
+# luarocks install luacheck
---
+++ runs/libs.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+sudo apt -y update
+
+# Essentials 
+sudo apt -y install git ripgrep pavucontrol xclip jq tldr shutter python3-pip
+
+git clone git@github.com:junegunn/fzf.git $HOME/Personal/fzf
+$HOME/Personal/fzf/install
+# Do you want to enable fuzzy auto-completion? ([y]/n) y
+# Do you want to enable key bindings? ([y]/n) y
+# Generate /home/user/.fzf.bash ... OK
+# Update fish_user_paths ... Failed
+# Do you want to update your shell configuration files? ([y]/n) y
+# Update /home/user/.bashrc:
+#   - [ -f ~/.fzf.bash ] && source ~/.fzf.bash
+#     + Added
+# Create /home/user/.config/fish/functions/fish_user_key_bindings.fish:
+#     function fish_user_key_bindings
+#       fzf --fish | source
+#     end
+# Finished. Restart your shell or reload config file.
+#    source ~/.bashrc  # bash
+#    fzf_key_bindings  # fish
+# Use uninstall script to remove fzf.
+# For more information, see: https://github.com/junegunn/fzf
+
+sudo apt -y install plocate
+cargo install gping navi tealdeer tokei yazi-cli
