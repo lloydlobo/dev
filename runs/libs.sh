41ecb56fc35a62dca36a2a4b8d3e50d38de9eb44
Lloyd Lobo <lloydlobo4@gmail.com>
2025-02-13 09:37:14 +0530

    runs: format docs in libs.sh

--- runs/libs.sh
+++ runs/libs.sh
@@ -2,28 +2,34 @@
 
 sudo apt -y update
 
-# Essentials 
-sudo apt -y install git ripgrep pavucontrol xclip jq tldr shutter python3-pip
+# Essentials
+sudo apt -y install git ripgrep pavucontrol xclip jq shutter python3-pip
+
+# Skip this as I am using tealdeer
+#   sudo apt -y install tldr
 
 git clone git@github.com:junegunn/fzf.git $HOME/Personal/fzf
 $HOME/Personal/fzf/install
-# Do you want to enable fuzzy auto-completion? ([y]/n) y
-# Do you want to enable key bindings? ([y]/n) y
-# Generate /home/user/.fzf.bash ... OK
-# Update fish_user_paths ... Failed
-# Do you want to update your shell configuration files? ([y]/n) y
-# Update /home/user/.bashrc:
-#   - [ -f ~/.fzf.bash ] && source ~/.fzf.bash
-#     + Added
-# Create /home/user/.config/fish/functions/fish_user_key_bindings.fish:
-#     function fish_user_key_bindings
-#       fzf --fish | source
-#     end
-# Finished. Restart your shell or reload config file.
-#    source ~/.bashrc  # bash
-#    fzf_key_bindings  # fish
-# Use uninstall script to remove fzf.
-# For more information, see: https://github.com/junegunn/fzf
+#   Do you want to enable fuzzy auto-completion? ([y]/n) y
+#   Do you want to enable key bindings? ([y]/n) y
+#   Generate /home/user/.fzf.bash ... OK
+#   Update fish_user_paths ... Failed
+#   Do you want to update your shell configuration files? ([y]/n) y
+#   Update /home/user/.bashrc:
+#     - [ -f ~/.fzf.bash ] && source ~/.fzf.bash
+#       + Added
+#   Create /home/user/.config/fish/functions/fish_user_key_bindings.fish:
+#       function fish_user_key_bindings
+#         fzf --fish | source
+#       end
+#   Finished. Restart your shell or reload config file.
+#      source ~/.bashrc  # bash
+#      fzf_key_bindings  # fish
+#   Use uninstall script to remove fzf.
+#   For more information, see: https://github.com/junegunn/fzf
+
+
+# Convenience
 
 sudo apt -y install plocate
 cargo install gping navi tealdeer tokei yazi-cli
