c540c96a14bc3c010dee8681cc4782d6edb44bed
Lloyd Lobo <lloydlobo4@gmail.com>
2025-02-13 10:56:29 +0530

    env: remove fish config
    Non-POSIX compliance uses more working memory than necessary

--- env/.config/fish/config.fish
+++
@@ -1,7 +0,0 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
-
-set -x GOROOT /usr/local/go
-set -x GOPATH $HOME/go
-set -x PATH $GOROOT/bin $GOPATH/bin $PATH
