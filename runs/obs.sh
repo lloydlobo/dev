1991aa8840cd53e7036b8601c07445d74c1a822d
Lloyd Lobo <lloydlobo4@gmail.com>
2025-06-30 10:48:35 +0530

    chore(runs/obs): update comments on re-activating virtual camera each time on boot

--- runs/obs.sh
+++ runs/obs.sh
@@ -49,22 +49,25 @@ v4l2-ctl --list-devices
 #    ffplay /dev/video10
 #
 
+# NOTE: Unless this is made permanent you gotta do it each time to activate
+#       virtual camera!
+#
 # If the issue is likely that your OBS virtual camera is trying to use
 # /dev/video0 which is probably your physical webcam. Let's fix this:
+#
+# WARN: /dev/video0 sometimes worked for me aswell
+#
 
 # Remove the module if loaded
-sudo modprobe -r v4l2loopback 
-
+sudo modprobe -r v4l2loopback
 # Load it with a specific video number that's not in use
 sudo modprobe v4l2loopback devices=1 video_nr=10 card_label="OBS Cam" exclusive_caps=1
-
 # You can check what device was created with:
 ls /dev/video*
 # You should now see both /dev/video0 (your real camera) and /dev/video10 (OBS virtual camera).
 v4l2-ctl --list-devices
 # This should show OBS output
 ffplay /dev/video10
-
 
 # Make it load automatically at boot
 echo 'v4l2loopback' | sudo tee /etc/modules-load.d/v4l2loopback.conf
